849161b0788173975ffd0e5eb33631e7efe5d9e1
Shane Soh <shanesoh@Shanes-Mac-Pro.local>
2015-07-31 02:29:02 -0700

    Fixed pep8

--- anticipatory-rnn/maneuver-anticipation/evaluatealgorithm.py
+++ anticipatory-rnn/maneuver-anticipation/evaluatealgorithm.py
@@ -1,11 +1,11 @@
 import numpy as np
-from maneuver_prediction_rnn import evaluate 
+from maneuver_prediction_rnn import evaluate
 import sys
 
 maneuver_type = sys.argv[1]
 checkpoint = sys.argv[2]
 
-checkpoints = [checkpoint,checkpoint,checkpoint,checkpoint,checkpoint]
+checkpoints = [checkpoint, checkpoint, checkpoint, checkpoint, checkpoint]
 
 folds = ['fold_1', 'fold_2', 'fold_3', 'fold_4', 'fold_5']
 
@@ -15,57 +15,56 @@ model_type = 'lstm_one_layer'
 path_to_load_from = ''
 append_to_checkpoint = ''
 if maneuver_type == 'all':
-	size = 5
-	model_type = 'multipleRNNs'
-	i = '356988'
-	index = [i,i,i,i,i]
-	checkpoints = ['350','420','420','350','400']
-	path_to_load_from = 'checkpoints/all'
-	append_to_checkpoint = '_multipleRNNs_lstm_size_64'
-	# Set threshold in neuralmodels/prediction.py = 0.76
+    size = 5
+    model_type = 'multipleRNNs'
+    i = '356988'
+    index = [i, i, i, i, i]
+    checkpoints = ['350', '420', '420', '350', '400']
+    path_to_load_from = 'checkpoints/all'
+    append_to_checkpoint = '_multipleRNNs_lstm_size_64'
+    # Set threshold in neuralmodels/prediction.py = 0.76
 
 elif maneuver_type == 'lane':
-	size = 3	
-	i = '723759'	
-	model_type = 'multipleRNNs'
-	index = [i,i,i,i,i]
-	checkpoints = ['200','420','200','250','200']
-	path_to_load_from = 'checkpoints/lane'
-	append_to_checkpoint = '_multipleRNNs_lstm_size_64'
-	# Set threshold in neuralmodels/prediction.py = 0.8
+    size = 3
+    i = '723759'
+    model_type = 'multipleRNNs'
+    index = [i, i, i, i, i]
+    checkpoints = ['200', '420', '200', '250', '200']
+    path_to_load_from = 'checkpoints/lane'
+    append_to_checkpoint = '_multipleRNNs_lstm_size_64'
+    # Set threshold in neuralmodels/prediction.py = 0.8
 
 elif maneuver_type == 'turns':
-	size = 3
-	model_type = 'multipleRNNs'
-	i = '209221'
-	index = [i,i,i,i,i]
-	checkpoints = ['400','400','500','400','400']
-	path_to_load_from = 'checkpoints/turns'
-	append_to_checkpoint = '_multipleRNNs_lstm_size_64'
-	# Set threshold in neuralmodels/prediction.py = 0.868
+    size = 3
+    model_type = 'multipleRNNs'
+    i = '209221'
+    index = [i, i, i, i, i]
+    checkpoints = ['400', '400', '500', '400', '400']
+    path_to_load_from = 'checkpoints/turns'
+    append_to_checkpoint = '_multipleRNNs_lstm_size_64'
+    # Set threshold in neuralmodels/prediction.py = 0.868
 else:
-	print "did not match any maneuver"
+    print "did not match any maneuver"
 
-confMat = np.zeros((size,size))
-pMat = np.zeros((size,size))
-rMat = np.zeros((size,size))
+confMat = np.zeros((size, size))
+pMat = np.zeros((size, size))
+rMat = np.zeros((size, size))
 
 avg_precision = []
 avg_recall = []
 avg_anticipation_time = []
 
-for i,f,checkpoint in zip(index,folds,checkpoints):
-	path_new = ''
-	if len(path_to_load_from) > 0:
-		checkpoint += append_to_checkpoint
-		path_new = path_to_load_from + '/' + f
-	[conMat,p_mat,re_mat,time_mat] = evaluate(i,f,checkpoint,model_type,path_new)
-	confMat += conMat
-	pMat += p_mat
-	rMat += re_mat
-	avg_precision.append(np.mean(np.diag(p_mat)[1:]))
-	avg_recall.append(np.mean(np.diag(re_mat)[1:]))
-	avg_anticipation_time.append(np.mean(  np.divide(np.diag(time_mat)[1:],np.diag(conMat)[1:])   ))
+for i, f, checkpoint in zip(index, folds, checkpoints):
+    path_new = ''
+    if len(path_to_load_from) > 0:
+        checkpoint += append_to_checkpoint
+    confMat += conMat
+    pMat += p_mat
+    rMat += re_mat
+    avg_precision.append(np.mean(np.diag(p_mat)[1:]))
+    avg_recall.append(np.mean(np.diag(re_mat)[1:]))
+    avg_anticipation_time.append(
+        np.mean(np.divide(np.diag(time_mat)[1:], np.diag(conMat)[1:])))
 
 avg_precision = np.array(avg_precision)
 avg_recall = np.array(avg_recall)
@@ -86,10 +85,10 @@ print "Time = {0} ({1})".format(np.mean(avg_anticipation_time),(np.std(avg_antic
 print '****************{0}****************'.format(checkpoint)
 print 'Precision'
 print avg_precision
-print "{0} ({1})".format(np.mean(avg_precision),(np.std(avg_precision)/np.sqrt(len(folds))))
+print "{0} ({1})".format(np.mean(avg_precision), (np.std(avg_precision)/np.sqrt(len(folds))))
 print ''
 
 print 'Recall'
 print avg_recall
-print "{0} ({1})".format(np.mean(avg_recall),(np.std(avg_recall)/np.sqrt(len(folds))))
+print "{0} ({1})".format(np.mean(avg_recall), (np.std(avg_recall)/np.sqrt(len(folds))))
 print ''
--- anticipatory-rnn/maneuver-anticipation/maneuver-rnn.py
+++ anticipatory-rnn/maneuver-anticipation/maneuver-rnn.py
@@ -4,93 +4,123 @@ import theano
 import os
 from theano import tensor as T
 from neuralmodels.utils import permute, load
-from neuralmodels.costs import softmax_decay_loss,softmax_loss
+from neuralmodels.costs import softmax_decay_loss, softmax_loss
 from neuralmodels.models import RNN, MultipleRNNsCombined
-from neuralmodels.predictions import OutputMaxProb, OutputSampleFromDiscrete,OutputActionThresh
-from neuralmodels.layers import softmax, simpleRNN, OneHot, LSTM, TemporalInputFeatures
+from neuralmodels.predictions import (
+    OutputMaxProb, OutputSampleFromDiscrete, OutputActionThresh)
+from neuralmodels.layers import (
+    softmax, simpleRNN, OneHot, LSTM, TemporalInputFeatures)
 from neuralmodels.updates import Adagrad, RMSprop
 import cPickle
 from utils import confusionMat
-from predictions import predictManeuver,predictLastTimeManeuver
+from predictions import predictManeuver, predictLastTimeManeuver
 
 if __name__ == '__main__':
 
-	index = sys.argv[1]
-	fold = sys.argv[2]	
-	path_to_dataset = '/scr/ashesh/brain4cars/dataset/{0}'.format(fold)
-	path_to_checkpoints = '/scr/ashesh/brain4cars/checkpoints/{0}'.format(fold)
-	test_data = cPickle.load(open('{1}/test_data_{0}.pik'.format(index,path_to_dataset)))	
-	Y_te = test_data['labels']
-	X_te = test_data['features']
-	actions = []
-	if test_data.has_key('actions'):
-		actions = test_data['actions']
-	else:
-		actions = ['end_action','lchange','rchange','lturn','rturn']
-	#print X_te.shape
-	#print Y_te.shape
+    index = sys.argv[1]
+    fold = sys.argv[2]
+    pwd = os.getcwd()
+    path_to_dataset = 'pwd/dataset/{0}'.format(fold)
+    path_to_checkpoints = 'pwd/checkpoints/{0}'.format(fold)
+    test_data = cPickle.load(
+        open('{1}/test_data_{0}.pik'.format(index, path_to_dataset)))
+    Y_te = test_data['labels']
+    X_te = test_data['features']
+    actions = []
+    if 'actions' in test_data:
+        actions = test_data['actions']
+    else:
+        actions = ['end_action', 'lchange', 'rchange', 'lturn', 'rturn']
+    # print X_te.shape
+    # print Y_te.shape
 
-	train_data = cPickle.load(open('{1}/train_data_{0}.pik'.format(index,path_to_dataset)))	
-	Y_tr = train_data['labels']
-	X_tr = train_data['features']
-	print X_tr.shape
-	print Y_tr.shape
+    train_data = cPickle.load(
+        open('{1}/train_data_{0}.pik'.format(index, path_to_dataset)))
+    Y_tr = train_data['labels']
+    X_tr = train_data['features']
+    print X_tr.shape
+    print Y_tr.shape
 
-	
-	print type(X_tr[0,0,0])
+    print type(X_tr[0, 0, 0])
 
-	num_train = X_tr.shape[1]
-	num_test = len(X_te)
-	len_samples = X_tr.shape[0]
+    num_train = X_tr.shape[1]
+    num_test = len(X_te)
+    len_samples = X_tr.shape[0]
 
-	num_classes = int(np.max(Y_tr) - np.min(Y_tr) + 1)
-	inputD = X_tr.shape[2]
-	outputD = num_classes 
+    num_classes = int(np.max(Y_tr) - np.min(Y_tr) + 1)
+    inputD = X_tr.shape[2]
+    outputD = num_classes
 
-	print 'Number of classes ',outputD
-	print 'Feature dimension ',inputD
+    print 'Number of classes ', outputD
+    print 'Feature dimension ', inputD
 
-	epochs = 600
-	batch_size = num_train
-	learning_rate_decay = 0.97
-	decay_after = 5
-	step_size = 6e-4
-		
-	use_pretrained = False 
-	train_more = False
-	global rnn
+    epochs = 600
+    batch_size = num_train
+    learning_rate_decay = 0.97
+    decay_after = 5
+    step_size = 6e-4
 
-	architectures = ['lstm_one_layer','lstm_two_layers','multipleRNNs']
-	model_type = 2
+    use_pretrained = False
+    train_more = False
+    global rnn
 
+    architectures = ['lstm_one_layer', 'lstm_two_layers', 'multipleRNNs']
+    model_type = 2
 
-	if not use_pretrained:
-		if not os.path.exists(path_to_checkpoints):
-			os.mkdir(path_to_checkpoints)
+    if not use_pretrained:
+        if not os.path.exists(path_to_checkpoints):
+            os.mkdir(path_to_checkpoints)
 
-		if not os.path.exists('{1}/{0}/'.format(index,path_to_checkpoints)):
-			os.mkdir('{1}/{0}/'.format(index,path_to_checkpoints))
+        if not os.path.exists('{1}/{0}/'.format(index, path_to_checkpoints)):
+            os.mkdir('{1}/{0}/'.format(index, path_to_checkpoints))
 
-		trY = T.lmatrix()
-		
-		# Creating network layers
-		if architectures[model_type] == 'lstm_one_layer':
-			layers = [TemporalInputFeatures(inputD),LSTM('tanh','sigmoid','orthogonal',4,32,None),softmax(num_classes)]
-			rnn = RNN(layers,softmax_decay_loss,trY,step_size,Adagrad())
-			rnn.fitModel(X_tr,Y_tr,1,'{1}/{0}/'.format(index,path_to_checkpoints),epochs,batch_size,learning_rate_decay,decay_after)
+        trY = T.lmatrix()
 
-		elif architectures[model_type] == 'lstm_two_layers':
-			layers = [TemporalInputFeatures(inputD),LSTM('tanh','sigmoid','orthogonal',4,32,None),LSTM('tanh','sigmoid','orthogonal',4,32,None),softmax(num_classes)]
-			rnn = RNN(layers,softmax_decay_loss,trY,step_size,Adagrad())
-			rnn.fitModel(X_tr,Y_tr,1,'{1}/{0}/'.format(index,path_to_checkpoints),epochs,batch_size,learning_rate_decay,decay_after)
+        # Creating network layers
+        if architectures[model_type] == 'lstm_one_layer':
+            layers = [TemporalInputFeatures(inputD),
+                      LSTM('tanh', 'sigmoid', 'orthogonal', 4, 32, None),
+                      softmax(num_classes)]
+            rnn = RNN(layers, softmax_decay_loss, trY, step_size, Adagrad())
+            rnn.fitModel(X_tr, Y_tr, 1,
+                         '{1}/{0}/'.format(index, path_to_checkpoints),
+                         epochs, batch_size, learning_rate_decay, decay_after)
 
-		elif architectures[model_type] == 'multipleRNNs':
-			road_features_dimension = 4
-			layers_1 = [TemporalInputFeatures(road_features_dimension)]
-			layers_2 = [TemporalInputFeatures(inputD-road_features_dimension),LSTM('tanh','sigmoid','orthogonal',4,64,None)]
-			#layers_2 = [TemporalInputFeatures(inputD),LSTM('tanh','sigmoid','orthogonal',4,32,None)]
-			output_layer = [simpleRNN('tanh','normal',4,64,temporal_connection=False),softmax(num_classes)]
-			rnn = MultipleRNNsCombined([layers_1,layers_2],output_layer,softmax_decay_loss,trY,step_size,Adagrad())
-			#[:,:,:inputD-road_features_dimension]
-			rnn.fitModel([X_tr[:,:,(inputD-road_features_dimension):],X_tr[:,:,:inputD-road_features_dimension]],Y_tr,1,'{1}/{0}/'.format(index,path_to_checkpoints),epochs,batch_size,learning_rate_decay,decay_after)
+        elif architectures[model_type] == 'lstm_two_layers':
+            layers = [TemporalInputFeatures(inputD),
+                      LSTM('tanh', 'sigmoid', 'orthogonal', 4, 32, None),
+                      LSTM('tanh', 'sigmoid', 'orthogonal', 4, 32, None),
+                      softmax(num_classes)]
+            rnn = RNN(layers, softmax_decay_loss, trY, step_size, Adagrad())
+            rnn.fitModel(X_tr, Y_tr, 1,
+                         '{1}/{0}/'.format(index, path_to_checkpoints),
+                         epochs, batch_size, learning_rate_decay, decay_after)
 
+        elif architectures[model_type] == 'multipleRNNs':
+            road_features_dimension = 4
+
+            layers_1 = [TemporalInputFeatures(road_features_dimension)]
+            layers_2 = [TemporalInputFeatures(
+                        inputD - road_features_dimension),
+                        LSTM('tanh', 'sigmoid', 'orthogonal', 4, 64, None)]
+            output_layer = [simpleRNN(
+                            'tanh',
+                            'normal',
+                            4, 64,
+                            temporal_connection=False),
+                            softmax(num_classes)]
+
+            rnn = MultipleRNNsCombined([layers_1, layers_2],
+                                       output_layer,
+                                       softmax_decay_loss,
+                                       trY,
+                                       step_size,
+                                       Adagrad())
+
+            rnn.fitModel([X_tr[:, :, (inputD-road_features_dimension):],
+                         X_tr[:, :, :inputD-road_features_dimension]],
+                         Y_tr, 1, '%s %s' % (index, path_to_checkpoints),
+                         epochs,
+                         batch_size,
+                         learning_rate_decay,
+                         decay_after)
