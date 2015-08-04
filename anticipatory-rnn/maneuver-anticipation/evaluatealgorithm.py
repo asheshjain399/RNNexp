067618205814c306a580b539dcd13df28ca9d2fd
Shane Soh <shanesoh@Shanes-Mac-Pro.local>
2015-08-04 05:43:56 -0700

    Pickles results as a DataFrame

--- anticipatory-rnn/maneuver-anticipation/evaluatealgorithm.py
+++ anticipatory-rnn/maneuver-anticipation/evaluatealgorithm.py
@@ -4,6 +4,7 @@ import sys
 import neuralmodels.predictions
 import pickle
 import os
+import pandas
 
 # Evaluates with various checkpoints and output thresholds to determine
 # most optimal prediction parameters
@@ -12,19 +13,18 @@ if __name__ == '__main__':
     checkpoints_params = np.append(np.arange(300, 599, 50), 599)
     thresh_params = np.arange(.6, .9, .01)
 
-    save_file = sys.argv[1]
+    save_file = sys.argv[2]
     maneuver_type = sys.argv[2]
     idx = sys.argv[3]
 
     folds = ['fold_1', 'fold_2', 'fold_3', 'fold_4', 'fold_5']
-    cur_best_eval = {}
+    results = {}
 
-    for fold in folds:
-        f1_score_best = 0
-        for th in thresh_params:
+    for th in thresh_params:
+        results_th = {}
+        for fold in folds:
             for cp in checkpoints_params:
-                print '*******%s*********' % fold
-                print 'Using thresh: %f and checkpoint: %s' % (th, cp)
+                print 'Predicting: th: %f, fold: %s, checkpoint: %s' % (th, fold, cp)
 
                 with open('settings.py','w') as f:
                     f.write('OUTPUT_THRESH = %f \n' % th)
@@ -35,18 +35,12 @@ if __name__ == '__main__':
 
                 precision = np.mean(np.diag(p_mat)[1:])
                 recall = np.mean(np.diag(re_mat)[1:])
-                f1_score = (2*precision*recall)/(precision+recall)
 
-                print('Precision: %s, recall: %s, '
-                      'f1-score: %s \n' % (precision, recall, f1_score))
+                results_th[(fold, cp)] = [(precision, recall)]
+        df_th = pandas.DataFrame(results_th).stack().reset_index(level=0, drop = True)
+        df_th['mean'] = df_th.apply(lambda x: (np.mean([x[i][0] for i in xrange(len(x))]), np.mean([x[i][1] for i in xrange(len(x))])), axis=1)
+        print(df_th)
 
-                if f1_score > f1_score_best:
-                    f1_score_best = f1_score
-                    cur_best_eval[fold] = {
-                        'threshold': th, 'checkpoint': cp,
-                        'precision': precision, 'recall': recall,
-                        'f1-score': f1_score}
-                    print('Storing best eval params for '
-                          '%s so far: %s \n' % (fold, cur_best_eval[fold]))
+        results[th] = df_th
 
-    pickle.dump(cur_best_eval, open(save_file, 'wb'))
+    pickle.dump(results, open(save_file, 'wb'))
