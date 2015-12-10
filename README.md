50592d347668b8d32d1539c3aaaf68e4d6da9663
Ashesh Jain <ashesh@napoli110.Stanford.EDU>
2015-12-09 16:52:20 -0800

    edits icra branch readme

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Install NeuralModels https://github.com/asheshjain399/NeuralModels
 ## Description
 
 This repo contains the source code for the following papers:
-* Sensory-fusion recurrent neural architecture for driver activity anticipation http://brain4cars.com The source code is in the directory anticipatory-rnn/maneuver-rnn
+* Sensory-fusion recurrent neural architecture for driver activity anticipation http://brain4cars.com The source code is in the directory anticipatory-rnn/maneuver-rnn. Checkout 'icraversion' branch of NeuralModels to reproduce our results. 
 
 * Structural-RNN (S-RNN) for doing deep learning over spatio-temporal graphs.  The source code is present in the directory crf_to_rnn. This source code takes input a spatio-temporal graph written in a text file and creates the S-RNN architecture. Currently the directory contains the code for human motion modeling on H3.6m data set. Our paper can be downloaded from http://www.cs.stanford.edu/people/ashesh/srnn
 
--- anticipatory-rnn/maneuver-anticipation/generateBestResults.py
+++ anticipatory-rnn/maneuver-anticipation/generateBestResults.py
@@ -42,7 +42,7 @@ def readModelFile(model_file):
 	return checkpoints,threshold,dataset_id,model
 
 def getResults(conMat,p_mat,re_mat,time_mat):
-	return np.mean(np.diag(p_mat)[1:]),np.mean(np.diag(re_mat)[1:]),np.mean(np.divide(np.diag(time_mat)[1:],np.diag(conMat)[1:]))
+	return np.mean(np.diag(p_mat)[1:]),np.mean(np.diag(re_mat)[1:]),np.mean(np.divide(np.diag(time_mat)[1:],np.diag(conMat)[1:])), (np.sum(conMat[1:,0])/np.sum(conMat[:,0]))
 
 def pathToDataset(maneuver_type,fold,dataset_id):
 	return 'checkpoints/{0}/{1}/test_data_{2}.pik'.format(maneuver_type,fold,dataset_id)
@@ -85,12 +85,14 @@ def getBestResults(maneuver_type,checkpoints,model_id,model_type,dataset_id):
 	keys = range(10)
 	keys.append('best')
 	precision = {}
+	false_pos_rate = {}
 	recall = {}
 	anticipation_time = {}
 	p_confMat = {}
 
 	for k in keys:
 		precision[k] = []
+		false_pos_rate[k] = []
 		recall[k] = []
 		anticipation_time[k] = []
 		p_confMat[k] = []	
@@ -104,8 +106,9 @@ def getBestResults(maneuver_type,checkpoints,model_id,model_type,dataset_id):
 		for k in conMat.keys():
 			if len(conMat[k]) == 0:
 				continue
-			p_,r_,t_ = getResults(conMat[k],p_mat[k],re_mat[k],time_mat[k])
+			p_,r_,t_,fp_ = getResults(conMat[k],p_mat[k],re_mat[k],time_mat[k])
 			precision[k].append(100.0*p_)
+			false_pos_rate[k].append(100.0*fp_)
 			recall[k].append(100.0*r_)
 			anticipation_time[k].append(t_)
 			if len(p_confMat[k]) == 0:
@@ -118,6 +121,7 @@ def getBestResults(maneuver_type,checkpoints,model_id,model_type,dataset_id):
 		precision[k] = np.array(precision[k])
 		recall[k] = np.array(recall[k])
 		anticipation_time[k] = np.array(anticipation_time[k])
+		false_pos_rate[k] = np.array(false_pos_rate[k])
 
 	keys = range(10)
 	keys = keys[::-1]
@@ -125,18 +129,22 @@ def getBestResults(maneuver_type,checkpoints,model_id,model_type,dataset_id):
 	F1_list = []
 	pr = 0.0
 	re = 0.0
+	fpr = 0.0
 	ant_time = 0.0
 	pr_std = 0.0
 	re_std = 0.0
+	fpr_std = 0.0
 	ant_time_std = 0.0
 	for k in keys:
 		if len(precision[k]) == 0:
 			continue
 		pr = np.mean(precision[k])
 		re = np.mean(recall[k])
+		fpr = np.mean(false_pos_rate[k])
 		ant_time = np.mean(anticipation_time[k])
 
 		pr_std_err = np.std(precision[k])/np.sqrt(len(precision[k]))
+		fpr_std_err = np.std(false_pos_rate[k])/np.sqrt(len(false_pos_rate[k]))
 		re_std_err = np.std(recall[k])/np.sqrt(len(precision[k]))
 		ant_time_std = np.std(anticipation_time[k])
 
@@ -145,7 +153,7 @@ def getBestResults(maneuver_type,checkpoints,model_id,model_type,dataset_id):
 	print precision['best']
 	print "****************"
 	print "Best results"
-	print "Pr={0:.4f}({1:.4f})  Re={2:.4f}({3:.4f}) t={4:.4f}({5:.4f})".format(pr,pr_std_err,re,re_std_err,ant_time,ant_time_std)
+	print "Pr={0:.4f}({1:.4f})  Re={2:.4f}({3:.4f}) t={4:.4f}({5:.4f}) fpr={6:.4f}({7:.4f})".format(pr,pr_std_err,re,re_std_err,ant_time,ant_time_std,fpr,fpr_std_err)
 	print "****************"
 	print "F1-score by varying time to maneuver"
 	print F1_list[:-1]
