fcf55a2a5431926679cbdf527168dd04ff36d259
Ashesh Jain <ashesh@napoli110.Stanford.EDU>
2016-01-25 10:29:37 -0800

    edits

--- structural_rnn/README.md
+++ structural_rnn/README.md
@@ -4,7 +4,7 @@
 
 Install [NeuralModels](https://github.com/asheshjain399/NeuralModels) and checkout its [srnn](https://github.com/asheshjain399/NeuralModels/tree/srnn) branch
 
-Download the motion capture data from [URL]. We originally obtained this data from [H3.6m] (http://vision.imar.ro/human3.6m/description.php) and processed it further to exponential map. If you use this data set then please cite the original authors of H3.6m data set. Also check their lisencing conditions. 
+Download the motion capture data from [data set](http://www.cs.stanford.edu/people/ashesh/h3.6m.zip). We originally obtained this data from [H3.6m](http://vision.imar.ro/human3.6m/description.php) and processed it further to exponential map. If you use this data set then please cite the original authors of H3.6m data set. Also check their lisencing conditions. This is released only for research purpose. 
 
 Open ```basedir``` file and edit the path to where you have saved the data set. We will use the same directory to save the checkpoints of our trained model. Size of each checkpoint can go upto few 100's MB. So make sure that you have enough disk space before you start training your own model.
 
--- structural_rnn/hyperParameterTuning.py
+++ structural_rnn/hyperParameterTuning.py
@@ -3,6 +3,9 @@ import os
 import copy
 import socket as soc
 from datetime import datetime
+import sys
+
+train_model = sys.argv[1]
 
 base_dir = open('basedir','r').readline().strip()
 gpus = [1]
@@ -11,49 +14,83 @@ gpus = [1]
 ## Hyper parameters for training S-RNN
 params = {}
 
-## These hyperparameters are OKAY to tweak. They will affect training, convergence etc.
-params['initial_lr'] = 1e-3
-params['decay_schedule'] = [1.5e3,4.5e3] # Decrease learning rate after these many iterations
-params['decay_rate_schedule'] = [0.1,0.1] # Multiply the current learning rate by this factor
-params['lstm_init'] = 'uniform' # Initialization of lstm weights
-params['fc_init'] = 'uniform' # Initialization of FC layer weights
-params['clipnorm'] = 25.0
-params['use_noise'] = 1
-params['noise_schedule'] = [250,0.5e3,1e3,1.3e3,2e3,2.5e3,3.3e3] # Add noise after these many iterations
-params['noise_rate_schedule'] = [0.01,0.05,0.1,0.2,0.3,0.5,0.7] # Variance of noise to add
-params['momentum'] = 0.99
-params['g_clip'] = 25.0
-params['truncate_gradient'] = 10 #100
-params['sequence_length'] = 150 # Length of each sequence fed to RNN
-params['sequence_overlap'] = 50 
-params['batch_size'] = 100
-params['lstm_size'] = 10 #512
-params['node_lstm_size'] = 10 #512
-params['fc_size'] = 10 #256
-params['snapshot_rate'] = 250 # Save the model after every 250 iterations
-params['train_for'] = 'final' 
-'''
-Possible options are ['eating','smoking','discussion','final','']
-'': Use this for validation and hyperparameter tuning
-'final': Will train on activities {eating, smoking, walking, discussion}
-'eating': Will only train on eating activity
-Look Process data file for more details
-'''
+if train_model == 'srnn':
+	## These hyperparameters are OKAY to tweak. They will affect training, convergence etc.
+	params['initial_lr'] = 1e-3
+	params['decay_schedule'] = [1.5e3,4.5e3] # Decrease learning rate after these many iterations
+	params['decay_rate_schedule'] = [0.1,0.1] # Multiply the current learning rate by this factor
+	params['lstm_init'] = 'uniform' # Initialization of lstm weights
+	params['fc_init'] = 'uniform' # Initialization of FC layer weights
+	params['clipnorm'] = 25.0
+	params['use_noise'] = 1
+	params['noise_schedule'] = [250,0.5e3,1e3,1.3e3,2e3,2.5e3,3.3e3] # Add noise after these many iterations
+	params['noise_rate_schedule'] = [0.01,0.05,0.1,0.2,0.3,0.5,0.7] # Variance of noise to add
+	params['momentum'] = 0.99
+	params['g_clip'] = 25.0
+	params['truncate_gradient'] = 10 #100
+	params['sequence_length'] = 150 # Length of each sequence fed to RNN
+	params['sequence_overlap'] = 50 
+	params['batch_size'] = 100
+	params['lstm_size'] = 10 #512
+	params['node_lstm_size'] = 10 #512
+	params['fc_size'] = 10 #256
+	params['snapshot_rate'] = 250 # Save the model after every 250 iterations
+	params['train_for'] = 'final' 
+	'''
+	Possible options are ['eating','smoking','discussion','final','']
+	'': Use this for validation and hyperparameter tuning
+	'final': Will train on activities {eating, smoking, walking, discussion}
+	'eating': Will only train on eating activity
+	Look Process data file for more details
+	'''
 
-## Tweak these hyperparameters only if you want to try out new models etc. This is only for 'Advanced' users
-params['use_pretrained'] = 0
-params['iter_to_load'] = 2500
-params['model_to_train'] = 'dra'
-params['crf'] = ''
-params['copy_state'] = 0
-params['full_skeleton'] = 1
-params['weight_decay'] = 0.0
-params['temporal_features'] = 0
-params['dra_type'] = 'simple'
-params['dataset_prefix'] = ''
-params['drop_features'] = 0
-params['drop_id'] = '9'
-params['subsample_data'] = 1
+	## Tweak these hyperparameters only if you want to try out new models etc. This is only for 'Advanced' users
+	params['use_pretrained'] = 0
+	params['iter_to_load'] = 2500
+	params['model_to_train'] = 'dra'
+	params['crf'] = ''
+	params['copy_state'] = 0
+	params['full_skeleton'] = 1
+	params['weight_decay'] = 0.0
+	params['temporal_features'] = 0
+	params['dra_type'] = 'simple'
+	params['dataset_prefix'] = ''
+	params['drop_features'] = 0
+	params['drop_id'] = '9'
+	params['subsample_data'] = 1
+
+elif train_model == 'lstm3lr' or train_model == 'erd':
+	## These hyperparameters are OKAY to tweak. They will affect training, convergence etc.
+	params['truncate_gradient'] = 100
+	params['sequence_length'] = 150
+	params['sequence_overlap'] = 50
+	params['batch_size'] = 100
+	params['lstm_size'] = 1000 # This paprameter is same as the one used by Fragkiadaki et al. ICCV'15
+	params['fc_size'] = 500 # This paprameter is same as the one used by Fragkiadaki et al. ICCV'15
+	params['use_noise'] = 1
+	params['noise_schedule'] = [250,0.5e3,1e3,1.3e3,2e3,2.5e3,3.3e3] # Add noise after these many iterations
+	params['noise_rate_schedule'] = [0.01,0.05,0.1,0.2,0.3,0.5,0.7] # Variance of noise to add
+	params['initial_lr'] = 1e-3
+	params['decay_schedule'] = [1.5e3,4.5e3] # Decrease learning rate after these many iterations
+	params['decay_rate_schedule'] = [0.1,0.1] # Multiply the current learning rate by this factor
+	params['train_for'] = 'final'
+	params['clipnorm'] = 25.0
+
+	params['use_pretrained'] = 0
+	params['iter_to_load'] = 1250
+	if train_model == 'lstm3lr':
+		params['model_to_train'] = 'lstm'
+	else:
+		params['model_to_train'] = 'malik'
+	params['snapshot_rate'] = 250
+	params['crf'] = ''
+	params['copy_state'] = 0
+	params['full_skeleton'] = 1
+	params['weight_decay'] = 0.0
+	params['temporal_features'] = 0
+	params['dataset_prefix'] = ''
+	params['drop_features'] = 0
+	params['drop_id'] = '9'
 
 
 '''
@@ -97,8 +134,9 @@ else:
 
 # Setting directory to dump trained models and then executing trainDRA.py
 
-if params['model_to_train'] == 'dra':
-	params['checkpoint_path'] = 'checkpoints_{0}_T_{2}_bs_{1}_tg_{3}_ls_{4}_fc_{5}_demo'.format(params['model_to_train'],params['batch_size'],params['sequence_length'],params['truncate_gradient'],params['lstm_size'],params['fc_size'])
+#if params['model_to_train'] == 'dra':
+
+params['checkpoint_path'] = 'checkpoints_{0}_T_{2}_bs_{1}_tg_{3}_ls_{4}_fc_{5}_demo'.format(params['model_to_train'],params['batch_size'],params['sequence_length'],params['truncate_gradient'],params['lstm_size'],params['fc_size'])
 path_to_checkpoint = base_dir + '/{0}/'.format(params['checkpoint_path'])
 if not os.path.exists(path_to_checkpoint):
 	os.mkdir(path_to_checkpoint)
